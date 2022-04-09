626dfcca7b9c6dbe885aaf8ea190ab83ac2e8c93
Alexandre Vaz <alexandrevaz332@gmail.com>
2022-04-08 21:01:19 -0300

    Teste2

--- Teste/src/Teste.java
+++ Teste/src/Teste.java
@@ -1,22 +1,62 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Collections;
 
     public class Teste {
+        Scanner s = new Scanner(System.in);
+
+
 
         public static void main(String args[]) {
+      
+          {
+      
+            System.out.println("Masculino");
+      
+            List<String> masculino = new ArrayList<String>();
+      
+            masculino.add("João da Silva");
+      
+            masculino.add("Antonio Sousa");
+      
+            masculino.add("Alexandre Vaz");
+      
+            System.out.println(masculino);
+      
+            System.out.println("");
+      
+          }
+      
+          Scanner s = new Scanner(System.in);
+      
+            {
+      
+              System.out.println("Feminino");
+      
+              List<String> feminino = new ArrayList<String>();
+      
+              feminino.add("Marli Tereza");
+      
+              feminino.add("Juliana Alves");
+      
+              feminino.add("Lúcia Ferreira");
+      
+              Collections.sort(feminino);
+      
+              System.out.println(feminino);
+      
+              System.out.println("");
+      
+            }
+      
+          }
+      
+    
+      
+      
 
-            List<Pessoa> lista = new ArrayList<Pessoa>();
-
-            Pessoa a = new Pessoa("João da Silva", "Masculino");
-            Pessoa b = new Pessoa("Julia Santos", "Feminino");
-            Pessoa c = new Pessoa("Márcia Ferreira", "Feminino");
-
-            lista.add(a);
-            lista.add(b);
-            lista.add(c);
-            System.out.println(lista);
-
-
-        }
+        
 
 
     }
